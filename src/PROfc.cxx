9dd83db4e6d5647f2b48cad2d6a3ce631701dbf0
Mark Ross-Lonergan <markrosslonergan@gmail.com>
2025-08-22 11:52:16 -0500

    removed some redundancies in PROfc I intorudced

--- src/PROfc.cxx
+++ src/PROfc.cxx
@@ -35,7 +35,7 @@ namespace PROfit {
         std::uniform_int_distribution<uint32_t> dseed(0, std::numeric_limits<uint32_t>::max());
         Eigen::VectorXf cached_seed_syst = Eigen::VectorXf::Zero(nparams);
         Eigen::VectorXf cached_seed_osc = Eigen::VectorXf::Zero(nparams);
-        
+
         for(size_t u = 0; u < args.todo; ++u) {
             log<LOG_INFO>(L"%1% | Thread #%2% Throw #%3%") % __func__ % args.thread % u;
             std::normal_distribution<float> d;
@@ -81,53 +81,39 @@ namespace PROfit {
 
             if(chi2_syst < chi2_osc && !args.gof_mode) {
                 log<LOG_WARNING>(L"%1% || Negative delta chi2 detected! chi2_syst=%2% < chi2_osc=%3%") % __func__ % chi2_syst % chi2_osc;
-                //Re-fit oscillation hypothesis using syst solution as seed
-                log<LOG_INFO>(L"%1% || Attempting refit of oscillation hypothesis with syst solution as seed") % __func__;
+                log<LOG_WARNING>(L"%1% || Attempting enhanced global search") % __func__;
 
-                PROfitter fitter_osc_retry(ub_osc, lb_osc, args.fitconfig, dseed(rng));
-                float chi2_osc_retry = fitter_osc_retry.Fit(*metric, fitter.best_fit);
+                PROfitterConfig enhanced_config = args.fitconfig;
+                enhanced_config.n_multistart *= 2;
+                enhanced_config.n_swarm_particles = std::max(10, enhanced_config.n_swarm_particles * 2);
+                enhanced_config.n_localfit = std::max(5, enhanced_config.n_localfit + 2);
 
-                if(chi2_osc_retry < chi2_osc) {
-                    log<LOG_INFO>(L"%1% || Refit improved chi2_osc from %2% to %3%") 
-                        % __func__ % chi2_osc % chi2_osc_retry;
-                    chi2_osc = chi2_osc_retry;
-                    fitter_osc.best_fit = fitter_osc_retry.best_fit;
+                PROfitter fitter_osc_enhanced(ub_osc, lb_osc, enhanced_config, dseed(rng));
+                float chi2_osc_enhanced = fitter_osc_enhanced.Fit(*metric, fitter.best_fit);
+
+                if(chi2_osc_enhanced < chi2_osc) {
+                    log<LOG_INFO>(L"%1% || Enhanced search improved chi2_osc from %2% to %3%") 
+                        % __func__ % chi2_osc % chi2_osc_enhanced;
+                    chi2_osc = chi2_osc_enhanced;
+                    fitter_osc.best_fit = fitter_osc_enhanced.best_fit;
                 }
-                if(chi2_syst < chi2_osc) {
-                    log<LOG_WARNING>(L"%1% || Delta chi2 still negative after refit. Attempting enhanced global search") % __func__;
 
-                    PROfitterConfig enhanced_config = args.fitconfig;
-                    enhanced_config.n_multistart *= 2;
-                    enhanced_config.n_swarm_particles = std::max(10, enhanced_config.n_swarm_particles * 2);
-                    enhanced_config.n_localfit = std::max(5, enhanced_config.n_localfit + 2);
+        } // end of cross-checks
+        cached_seed_osc = fitter_osc.best_fit;    
 
-                    PROfitter fitter_osc_enhanced(ub_osc, lb_osc, enhanced_config, dseed(rng));
-                    float chi2_osc_enhanced = fitter_osc_enhanced.Fit(*metric, fitter.best_fit);
+        Eigen::VectorXf t = Eigen::VectorXf::Map(throws.data(), throws.size());
 
-                    if(chi2_osc_enhanced < chi2_osc) {
-                        log<LOG_INFO>(L"%1% || Enhanced search improved chi2_osc from %2% to %3%") 
-                            % __func__ % chi2_osc % chi2_osc_enhanced;
-                        chi2_osc = chi2_osc_enhanced;
-                        fitter_osc.best_fit = fitter_osc_enhanced.best_fit;
-                    }
+        args.out->push_back({
+                chi2_syst, chi2_osc, 
+                args.gof_mode ? 0 : std::pow(10.0f, fitter_osc.best_fit(0)), 
+                args.gof_mode ? 0 : std::pow(10.0f, fitter_osc.best_fit(1)), 
+                fitter.best_fit.segment(2, nparams - 2) , 
+                args.gof_mode ? Eigen::VectorXf() : fitter_osc.best_fit.segment(2, nparams-2) , t
+                });
 
-                }
-            } // end of cross-checks
-            cached_seed_osc = fitter_osc.best_fit;    
-
-            Eigen::VectorXf t = Eigen::VectorXf::Map(throws.data(), throws.size());
-
-            args.out->push_back({
-                    chi2_syst, chi2_osc, 
-                    args.gof_mode ? 0 : std::pow(10.0f, fitter_osc.best_fit(0)), 
-                    args.gof_mode ? 0 : std::pow(10.0f, fitter_osc.best_fit(1)), 
-                    fitter.best_fit.segment(2, nparams - 2) , 
-                    args.gof_mode ? Eigen::VectorXf() : fitter_osc.best_fit.segment(2, nparams-2) , t
-                    });
-
-            args.dchi2s->push_back( args.gof_mode ? -999 :std::abs(chi2_syst - chi2_osc ));
-            delete metric;
-        }
-    };
+        args.dchi2s->push_back( args.gof_mode ? -999 :std::abs(chi2_syst - chi2_osc ));
+        delete metric;
+    }
+};
 
 }
