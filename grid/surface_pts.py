4673ff3ffcf9689ad896950fe812b297f961f297
Jacob Larkin <jacob.larkin@stonybrook.edu>
2025-12-09 19:09:18 -0600

    Bug when copying over surface_pts.py. Should be log

--- grid/surface_pts.py
+++ grid/surface_pts.py
@@ -1,7 +1,7 @@
 import math
 
-xlims = (1e-2, 1)
-ylims = (1e-2, 100)
+xlims = (-2, 0)
+ylims = (-2, 2)
 
 xbins = 60
 ybins = 60
@@ -9,6 +9,6 @@ ybins = 60
 with open('surface_pts.txt', 'w') as f:
   for xi in range(xbins):
     for yi in range(ybins):
-      x = xlims[0] + xi*(xlims[1] - xlims[0])/xbins
-      y = ylims[0] + yi*(ylims[1] - ylims[0])/ybins
-      f.write(f'{x} {y}\n')
+      x = xlims[0] + float(xi)*(xlims[1] - xlims[0])/xbins
+      y = ylims[0] + float(yi)*(ylims[1] - ylims[0])/ybins
+      f.write(f'{10**x} {10**y}\n')
